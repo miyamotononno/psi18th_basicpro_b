834c199d8a008d9e8b0253bbcce50a506cd5a5c2
大村元気 <oomuramotoki@oomuramotoki-no-MacBook-Pro.local>
2018-05-23 20:18:05 +0900

    感情分析API

--- twitter_test.py
+++ twitter_test.py
@@ -13,6 +13,9 @@ import json
 import sys
 import config
 
+import urllib
+from urllib import request, parse
+
 oath_key_dict = {
     "consumer_key": config.consumer_key,
     "consumer_secret": config.consumer_secret,
@@ -20,7 +23,7 @@ oath_key_dict = {
     "access_token_secret": config.access_token_secret
 }
 
-max_count = 1000
+max_count = 200
 
 # 使わない、参考に
 '''
@@ -55,13 +58,14 @@ def get_text(word):
             text_list.append(tweet[u'text'])
             if i == 99:
                 max_id = int(tweet[u'id_str'])-1
-        print(len(set(text_list)))
-        if len(set(text_list)) >= max_count or not len(tweets["statuses"])==1000:
+        sys.stdout.write("\rTweet取得中... %d" % len(set(text_list)))
+        sys.stdout.flush()
+        if len(set(text_list)) >= max_count or not len(tweets["statuses"])==100:
             break
     # 重複や空を削除
     text_list = list(set(text_list))
     text_list = text_list[:max_count]
-    print(len(text_list))
+    print("\n取得数",len(text_list))
     return text_list
 
 
@@ -91,6 +95,53 @@ def tweet_search(search_word, oath_key_dict, max_id=0):
     tweets = json.loads(responce.text)
     return tweets
 
+def posi_or_nega(text):
+    # 基本URI
+    url = 'http://ap.mextractr.net/ma9/emotion_analyzer?'
+
+    # URIパラメータのデータ
+    # appkeyは個々人で取得してください
+    param = {
+        'out': 'json',
+        'apikey': '8D0342623B1801F4E3262E2166E70BB8AA269E5A',
+        'text': text
+    }
+
+    # URIパラメータの文字列の作成
+    #  Getリクエストの形式に整形  (text=アクセスが拒否されました。がっかり。&out=json&appley=8D0342623B1801F4E3262E2166E70BB8AA269E5A)
+    paramStr = urllib.parse.urlencode(param)
+
+    # 読み込むオブジェクトの作成
+    readObj = urllib.request.urlopen(url + paramStr)
+
+    # webAPIからのJSONを取得
+    res = readObj.read().decode()
+    #print(res)
+    # webAPIから取得したJSONデータをpythonで使える形に変換する
+    data = json.loads(res)
+
+    posi_or_nega = data['likedislike'] + data['joysad'] - abs(data['angerfear'])
+
+    return posi_or_nega
+
+
+def main(word):
+    texts = get_text(word)
+    emotion = []
+    for text in texts:
+        p_n = posi_or_nega(text)
+        emotion.append(p_n)
+        sys.stdout.write("\r感情分析中... %d" % len(emotion))
+        sys.stdout.flush()
+    posi_nega_neutral = [0,0,0]
+    for i in emotion:
+        if i >0:
+            posi_nega_neutral[0] += 1
+        elif i<0:
+            posi_nega_neutral[1] += 1
+        else:
+            posi_nega_neutral[2] += 1
+    return posi_nega_neutral
+
 if __name__ == "__main__":
-    #main(sys.argv[1])
-    get_text(sys.argv[1])
+    print(main(sys.argv[1]))
