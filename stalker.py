20cf1428a45182915954dbf3c3b0750564e55750
WataruOkamoto <watarokamoto@gmail.com>
2018-05-24 17:22:22 +0900

    '22件習得可能'

--- stalker.py
+++ stalker.py
@@ -1,17 +1,21 @@
 from bs4 import BeautifulSoup
 import urllib.request as req
 from selenium import webdriver
+import time
 
 url = "https://www.instagram.com/explore/tags/%E5%8F%8B%E9%81%94/"
 link = req.urlopen(url)
-# us_data = req.urlopen(url)
-# print(us_data.read().decode('utf-8'))
 
-soup = BeautifulSoup(link, "html.parser")
+browser = webdriver.Chrome('C:\selenium\chromedriver')
+browser.get(url)
+scroll_count = 0
 
-# 任意のデータを抽出
-res1 = soup.find("body")
-res2 = res1.find("span")
-print(res1)
+for i in range(0,10):
+   if scroll_count == 0:
+      time.sleep(1)
 
-
+soup = BeautifulSoup(browser.page_source, 'html.parser')
+res1 = soup.findAll('img')
+for elm in res1:
+   print(elm['alt'])
+print(len(res1))
