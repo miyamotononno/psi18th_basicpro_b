bc2df5093e789e6b3fa97a27d076c874ac59d271
miyamotononno <miyamoto-nozomu@g.ecc.u-tokyo.ac.jp>
2018-05-27 02:37:44 +0900

    俺もう疲れたわ...

--- static/js/Function.js
+++ static/js/Function.js
@@ -1,13 +1,17 @@
-
 var positiveValue = 1;
 var neutralValue = 1;
 var negativeValue = 1;
+var bar_label1 = "一位";
+var bar_label2 = "二位";
+var bar_label3 = "三位";
+var bar_count1 = 0;
+var bar_count2 = 0;
+var bar_count3 = 0;
 
-var input_value = 1;
+var input_value = "twitter";
 function onButtonSubmit(){
 
   input_value = document.form1.inputboxname.value;
-  console.log(input_value);
   $.ajax({
     type: 'post',
     url: "/twitter",
@@ -16,18 +20,24 @@ function onButtonSubmit(){
     dataType: 'JSON',
     scriptCharset: 'utf-8',
     success: function (data) {
-    //  alert(data['result']);
-      console.log(data['result'][0])
-      console.log(data['result'][0][0])
-      positiveValue = data['result'][0][0]
-      negativeValue = data['result'][0][1]
-      neutralValue = data['result'][0][2]
+      positiveValue = data['result'][0][0];
+      negativeValue = data['result'][0][1];
+      neutralValue = data['result'][0][2];
+      bar_label1 = data['result'][1][0][0];
+      bar_label2 = data['result'][1][1][0];
+      bar_label3 = data['result'][1][2][0];
+      bar_count1 = data['result'][1][0][1];
+      bar_count2 = data['result'][1][1][1];
+      bar_count3 = data['result'][1][2][1];
       piechart.data.datasets[0].data[0] = positiveValue;
       piechart.data.datasets[0].data[1] = negativeValue;
       piechart.data.datasets[0].data[2] = neutralValue;
-      barchart.data.datasets[0].data[0] = positiveValue;
-      barchart.data.datasets[0].data[1] = negativeValue;
-      barchart.data.datasets[0].data[2] = positiveValue;
+      barchart.data.labels[0] = bar_label1
+      barchart.data.labels[1] = bar_label2
+      barchart.data.labels[2] = bar_label3
+      barchart.data.datasets[0].data[0] = bar_count1;
+      barchart.data.datasets[0].data[1] = bar_count2;
+      barchart.data.datasets[0].data[2] = bar_count3;
       piechart.update();
       barchart.update();
     },
@@ -35,7 +45,6 @@ function onButtonSubmit(){
       console.error("error post clip");
     }
   });
-//  console.log(data['result'][0][1])
   return false;
 }
 
@@ -55,13 +64,14 @@ var piechart = new Chart(document.getElementById("myPiechart"), {
     ]
   }
 })
+console.log(bar_label3);
 
 var barchart = new Chart(document.getElementById("myBargraph"),{
     type: "bar",
     data:{
-      labels: ['1位','2位','3位'],
+      labels: [bar_label1, bar_label2, bar_label3],
       datasets: [{
-        data: [positiveValue ,negativeValue,neutralValue],
+        data: [bar_count1,bar_count2,bar_count3],
         backgroundColor: ['#FF4444', '#4444FF', '#44BB44', '#FFFF44', '#FF44FF']
       }]
     },
@@ -96,7 +106,7 @@ var barchart = new Chart(document.getElementById("myBargraph"),{
          categoryPercentage: 0.4,      //棒グラフ幅
          scaleLabel: {                 //軸ラベル設定
             display: true,             //表示設定
-            labelString: '横軸ラベル',  //ラベル
+            labelString: '関連ワード',  //ラベル
             fontSize: 18               //フォントサイズ
          },
          ticks: {
