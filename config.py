c7af7d8d6284d23fba152bcbbdfaea2244cdb303
miyamotononno <miyamoto-nozomu@g.ecc.u-tokyo.ac.jp>
2018-07-11 16:52:50 +0900

    Add config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+consumer_key= os.envron["COMSUMRE_KEY"]
+consumer_secret= os.envron["COMSUMRE_SECRET"]
+access_token= os.envron["ACCESS_TOKEN"]
+access_token_secret= os.envron["ACCESS_TOKEN_SECRET"]
